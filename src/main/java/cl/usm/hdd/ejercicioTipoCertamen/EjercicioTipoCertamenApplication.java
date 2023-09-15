d12ac4331dd7ef777ed043aaa5453bb0450ea105
Franco Mangini <francoman71@gmail.com>
2023-09-15 02:59:19 -0300

    inicial

---
+++ src/main/java/cl/usm/hdd/ejercicioTipoCertamen/EjercicioTipoCertamenApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.ejercicioTipoCertamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioTipoCertamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioTipoCertamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usm/hdd/ejercicioTipoCertamen/EjercicioTipoCertamenApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.ejercicioTipoCertamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTipoCertamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
